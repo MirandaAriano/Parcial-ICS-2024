ba3e06104d83bc8a7d9ed5dbc7177639c3e45371
Miranda Ariano <arianogabriel02@gmail.com>
2024-06-08 01:24:19 -0300

    Commit 9

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,2 +1,2 @@
 const verificacion = require('../HolaMundo/script')
-test('Prueba de Hola Mundo', () => { expect(verificacion('Hola Mundo')).toBe(true); });
+test('Prueba de Hola Mundo', () => { expect(verificacion('Hola Pedro')).toBe(true); });
