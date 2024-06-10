be3a06a85f20d531eead30d8b23d655512d35c05
MirandaAriano <MirandaAriano@users.noreply.github.com>
2024-06-10 17:14:59 +0000

    deploy: eef44df4524f8bcb6f6b775f818a9da5998446bd

---
+++ script.js
@@ -0,0 +1,18 @@
+function verificacion(input) {
+    if (input === "Hola Mundo") {
+        return (true)
+    } else {
+        return (false)
+    }
+}
+
+function verificarTexto() {
+    const input = document.getElementById('inputText').value;
+    if (verificacion(input) === true) {
+        document.body.style.backgroundColor = "green";
+    } else {
+        document.body.style.backgroundColor = "red";
+    }
+}
+
+module.exports = verificacion;
