ca916b080702e5b4adae40a5bda97b15e71cf09f
MirandaAriano <MirandaAriano@users.noreply.github.com>
2024-06-12 06:10:33 +0000

    deploy: 428be3812225945ee4a4e90b1b97792951bdaac4

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function verificarTexto() {
     if (verificacion(input) === true) {
         document.body.style.backgroundColor = "green";
     } else {
-        document.body.style.backgroundColor = "blue";
+        document.body.style.backgroundColor = "red";
     }
 }
 
